8e5d65f404ef800e7fe4d5714940334da586ceeb
thespaceadama <77792806+thespaceadama@users.noreply.github.com>
2021-01-21 22:31:12 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+number_1 = int(input('Ввведите первое число: '))
+number_2 = int(input('Ввведите второе число: '))
+number_3 = int(input('Ввведите третье число: '))
+number_4 = int(input('Ввведите четвертое число: '))
+
+result = number_1 + number_2 + number_3 + number_4
+print(number_1, '+', number_2, '+', number_3, '+', number_4, '=', result)
+
+result_2 = number_1 - number_2 - number_3 - number_4
+print(number_1, '-', number_2, '-', number_3, '-', number_4, '=', result_2)
+
+result_3 = number_1 * number_2 * number_3 * number_4
+print(number_1, '*', number_2, '*', number_3, '*', number_4, '=', result_3)
+
+result_6 = number_1 / number_2 / number_3 / number_4
+print(number_1, '/', number_2, '/', number_3, '/', number_4, '=', result_6)
+
---
+++ reverse.py
@@ -0,0 +1,8 @@
+x = 10
+y = 120
+
+#z = x
+#x = y
+#y = z
+x,y=y,x
+print('x =', x, 'y =', y)
