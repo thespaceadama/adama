c4ca2a426ca93c1d7f1d343feb00473bc24d28fd
thespaceadama <77792806+thespaceadama@users.noreply.github.com>
2021-01-22 17:46:00 +0600

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -2,16 +2,22 @@ number_1 = int(input('Ввведите первое число: '))
 number_2 = int(input('Ввведите второе число: '))
 number_3 = int(input('Ввведите третье число: '))
 number_4 = int(input('Ввведите четвертое число: '))
-
 result = number_1 + number_2 + number_3 + number_4
 print(number_1, '+', number_2, '+', number_3, '+', number_4, '=', result)
 
-result_2 = number_1 - number_2 - number_3 - number_4
-print(number_1, '-', number_2, '-', number_3, '-', number_4, '=', result_2)
+result = number_1 - number_2 - number_3 - number_4
+print(number_1, '-', number_2, '-', number_3, '-', number_4, '=', result)
 
-result_3 = number_1 * number_2 * number_3 * number_4
-print(number_1, '*', number_2, '*', number_3, '*', number_4, '=', result_3)
+result = number_1 * number_2 * number_3 * number_4
+print(number_1, '*', number_2, '*', number_3, '*', number_4, '=', result)
 
-result_6 = number_1 / number_2 / number_3 / number_4
-print(number_1, '/', number_2, '/', number_3, '/', number_4, '=', result_6)
+result = number_1 / number_2 / number_3 / number_4
+print(number_1, '/', number_2, '/', number_3, '/', number_4, '=', result)
+
+result = number_1 // number_2 // number_3 // number_4
+print(number_1, '//', number_2, '//',  number_3, '//', number_4, '=', result)
+
+result = number_1 % number_2 % number_3 % number_4
+print(number_1, '%', number_2, '%', number_3, '%', number_4, '=', result)
+
 
