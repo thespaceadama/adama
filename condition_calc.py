d33f94bb5183300839bf63ae5b94237e2a02b9ba
thespaceadama <oomatbekovaa@gmail.com>
2021-01-22 17:01:06 +0600

    second commit

---
+++ condition_calc.py
@@ -0,0 +1,21 @@
+print('+ : Плюс')
+print('- : Минус')
+print('* : Умножение')
+print('/ : Деление')
+select = input('Введите знак для операции: ')
+number_1 = int(input('Ввведите первое число: '))
+number_2 = int(input('Ввведите второе число: '))
+number_3 = int(input('Ввведите третье число: '))
+number_4 = int(input('Ввведите четвертое число: '))
+if select == '+':
+    result = number_1 + number_2 + number_3 + number_4
+    print(number_1, '+', number_2, '+', number_3, '+', number_4, '=', result)
+if select == '-':
+    result = number_1 - number_2 - number_3 - number_4
+    print(number_1, '-', number_2, '-', number_3, '-', number_4, '=', result)
+if select == '*':
+    result = number_1 * number_2 * number_3 * number_4
+    print(number_1, '*', number_2, '*', number_3, '*', number_4, '=', result)
+if select == '/':
+    result = number_1 / number_2 / number_3 / number_4
+    print(number_1, '/', number_2, '/', number_3, '/', number_4, '=', result)
