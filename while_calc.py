49ffa6b39d276cfe5982f350c566b503abb17251
thespaceadama <oomatbekovaa@gmail.com>
2021-01-26 22:36:16 +0600

    homework while calc

--- while_calc.py
+++ while_calc.py
@@ -2,20 +2,27 @@ while True:
     try:
         number_1 = int(input("введите 1 число: "))
         number_2 = int(input("введите 2 число: "))
-        operation = input("Какую операцию провести +, -, /, *, :")
-
+        number_3 = int(input("введите 3 число: "))
+        number_4 = int(input("введите 4 число: "))
+        operation = input("Какую операцию провести +, -, /, *, %, //: ")
 
         if operation == "+":
-            result = number_1 + number_2
+            result = number_1 + number_2 + number_3 + number_4
             print(result)
         elif operation == "-":
-            result = number_1 - number_2
+            result = number_1 - number_2 - number_3 - number_4
             print(result)
         elif operation == "/":
-            result = number_1 / number_2
+            result = number_1 / number_2 / number_3 / number_4
             print(result)
         elif operation == "*":
-            result = number_1 * number_2
+            result = number_1 * number_2 * number_3 * number_4
+            print(result)
+        elif operation == "%":
+            result = number_1 % number_2 % number_3 % number_4
+            print(result)
+        elif operation == "//":
+            result = number_1 // number_2 // number_3 // number_4
             print(result)
         else:
             print("введен неверный знак операции")
